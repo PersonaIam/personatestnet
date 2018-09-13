365cfaf1610a67003760b44082bb69a1b9be6616
vlad.tomsa <vlad.tomsa@persona.im>
2018-09-13 11:21:59 +0300

    Small refactor on IPFS module
    Signed-off-by: vlad.tomsa <vlad.tomsa@persona.im>

--- modules/ipfs.js
+++ modules/ipfs.js
@@ -16,7 +16,7 @@ let modules, library, self, shared = {};
 let __private = {};
 
 // Constructor
-function IPFSModule (cb, scope) {
+function IPFSModule(cb, scope) {
     library = scope;
     self = this;
     self.ipfsFactory = IPFSFactory.create(IPFS_FACTORY_OPTIONS);
@@ -26,10 +26,12 @@ function IPFSModule (cb, scope) {
 
 // Private methods
 __private.attachApi = function () {
-    var router = new Router();
+    const router = new Router();
 
     router.use(function (req, res, next) {
-        if (modules) { return next(); }
+        if (modules) {
+            return next();
+        }
         res.status(500).send({success: false, error: 'Blockchain is loading'});
     });
 
@@ -44,13 +46,40 @@ __private.attachApi = function () {
 
     library.network.app.use('/api/ipfs', router);
     library.network.app.use(function (err, req, res, next) {
-        if (!err) { return next(); }
+        if (!err) {
+            return next();
+        }
         library.logger.error('API error ' + req.url, err);
         res.status(500).send({success: false, error: 'API error: ' + err.message});
     });
 };
 
-__private.addHashToQueue = function(ipfsHash, cb) {
+__private.addFilesToIpfs = function (files, cb) {
+    try {
+        const {path, content} = files;
+
+        self.ipfsDaemon.api.files.add({path, content: Buffer.from(content)}, (err, filesAdded) => {
+            if (err) throw err;
+
+            library.logger.info('# Files added to IPFS');
+
+            const hash = filesAdded[0].hash;
+
+            __private.addHashToQueue(hash, function (error, data) {
+                if (error) throw error;
+
+                library.logger.info(`# ${data.queuedHash} added in queue`);
+
+                return (cb(null, {hash: data.queuedHash}));
+            });
+        });
+    }
+    catch (error) {
+        return (cb(error.message, null));
+    }
+};
+
+__private.addHashToQueue = function (ipfsHash, cb) {
     let params = {};
     params.ipfsHash = ipfsHash;
     params.timestamp = slots.getTime();
@@ -67,12 +96,12 @@ __private.addHashToQueue = function(ipfsHash, cb) {
 
             return cb(null, data);
         }).catch(function (err) {
-            library.logger.error("stack", err.stack);
-            return cb('IPFS Queue insert error');
-        })
+        library.logger.error("stack", err.stack);
+        return cb('IPFS Queue insert error');
+    })
 };
 
-__private.getHashFromQueue = function(ipfsHash, cb) {
+__private.getHashFromQueue = function (ipfsHash, cb) {
     library.db.query(sql.IpfsSql.getHashFromQueue, {ipfsHash})
         .then(function (rows) {
             let data = {};
@@ -86,12 +115,12 @@ __private.getHashFromQueue = function(ipfsHash, cb) {
 
             return cb(null, data);
         }).catch(function (err) {
-            library.logger.error("stack", err.stack);
-            return cb('IPFS Queue get error');
-        })
+        library.logger.error("stack", err.stack);
+        return cb('IPFS Queue get error');
+    })
 };
 
-__private.removeHashFromQueue = function(id, cb) {
+__private.removeHashFromQueue = function (id, cb) {
     library.db.query(sql.IpfsSql.deleteHash, {id})
         .then(function (rows) {
             return cb(null, rows);
@@ -111,54 +140,49 @@ __private.pin = function (hash, cb) {
     });
 };
 
+__private.getFile = function (hash, cb) {
+    self.ipfsDaemon.api.files.cat(hash, (err, file) => {
+        if (err) return cb(err);
+
+        const fileData = file.toString();
+
+        return cb(null, {fileData});
+    });
+};
+
 //
 //__API__ `addFilesToIpfs`
 shared.addFilesToIpfs = function (req, cb) {
-    try {
-        const { path, content } = req.body.files;
+    library.schema.validate(req.body, schema.addFilesToIpfs, function (err) {
+        if (err) return cb(err[0].message);
 
-        self.ipfsDaemon.api.files.add({ path, content: Buffer.from(content) }, (err, filesAdded) => {
-            if (err) throw err;
+        __private.addFilesToIpfs(req.body.files, function (error, data) {
+            if (error) return (cb(error.message, null));
 
-            library.logger.info('# Files added to IPFS');
-
-            const hash = filesAdded[0].hash;
-
-            __private.addHashToQueue(hash, function(error, data) {
-                if (error) throw error;
-
-                library.logger.info(`# ${data.queuedHash} added in queue`);
-
-                return (cb(null, { hash: data.queuedHash }));
-            });
+            return cb(null, data);
         });
-    }
-    catch (error) {
-        return (cb(error.message, null));
-    }
+    });
 };
 
 //
 //__API__ `getFileFromIpfs`
 shared.getFileFromIpfs = function (req, cb) {
     library.schema.validate(req.body, schema.getFileFromIpfs, function (err) {
-        if (err) { return cb(err[0].message); }
+        if (err) {
+            return cb(err[0].message);
+        }
 
-        const { hash } = req.body;
+        __private.getFile(req.body.hash, function (error, data) {
+            if (error) return (cb(error.message, null));
 
-        self.ipfsDaemon.api.files.cat(hash, (err, file) => {
-            if (err) return cb(err.message);
-
-            const fileData = file.toString();
-
-            return cb(null, { fileData });
+            return cb(null, data);
         });
     });
 };
 
 //
 //__EVENT__ `onStartIpfs`
-IPFSModule.prototype.onStartIpfs = function() {
+IPFSModule.prototype.onStartIpfs = function () {
     // Spawn an IPFS daemon
     self.ipfsFactory.spawn((error, ipfsDaemon) => {
         if (error) console.log(error);
@@ -193,15 +217,15 @@ IPFSModule.prototype.cleanup = function (cb) {
 };
 
 IPFSModule.prototype.apply = function (queuedHash, cb) {
-    __private.getHashFromQueue(queuedHash, function(error, res) {
+    __private.getHashFromQueue(queuedHash, function (error, res) {
         if (error) cb(error, null);
 
-        const { id, hash } = res;
+        const {id, hash} = res;
 
-        __private.pin(hash, function(err) {
+        __private.pin(hash, function (err) {
             if (err) cb(err, null);
 
-            __private.removeHashFromQueue(id, function(err, res) {
+            __private.removeHashFromQueue(id, function (err, res) {
                 if (err) cb(err, null);
 
                 cb(null, res);
--- schema/ipfs.js
+++ schema/ipfs.js
@@ -1,6 +1,17 @@
 'use strict';
 
 module.exports = {
+    addFilesToIpfs: {
+        id: 'ipfs.addFilesToIpfs',
+        type: 'object',
+        properties: {
+            files: {
+                type: 'string',
+                minLength: 1
+            },
+        },
+        required: ['files'],
+    },
     getFileFromIpfs: {
         id: 'ipfs.getFileFromIpfs',
         type: 'object',
